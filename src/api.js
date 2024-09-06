1b353874bc938078a3ba18d7f69216c594db2107
Prateek Passi <passi264@gmail.com>
2024-09-06 14:37:15 -0400

    hi

--- src/api.js
+++ src/api.js
@@ -1,101 +1,210 @@
-import axios from "axios"
-const URL= "https://mongobase-two.vercel.app/"
-export async function getPosts(){
-    const response = await axios.get(`${URL}/posts`)
-    if (response.status===200){
-    return response.data
-    } else{
-        return
+import axios from "axios";
+
+// Removed trailing slash from the base URL
+const URL = "https://mongobase-two.vercel.app";
+
+// Function to get all posts
+export async function getPosts() {
+    const response = await axios.get(`${URL}/posts`);
+    if (response.status === 200) {
+        return response.data;
+    } else {
+        return;
     }
 }
 
+// Function to get a specific post by ID
 export async function getPost(id) {
-    //"http://localhost:3000/posts/12345"
-    const response = await axios.get(`${URL}/posts/${id}`)
-
-    const post = response.data
-    const data = await getImage(post.imageId)
-    post.image = data
-    return post
+    const response = await axios.get(`${URL}/posts/${id}`);
+    const post = response.data;
+    const data = await getImage(post.imageId);
+    post.image = data;
+    return post;
 }
 
-
-export async function createPost(post){
-    console.log(post)
-    const data= await createImage(post.file)
-    const imageId = post.file.name
-    post.imageId= imageId
-    const response = await axios.post(`${URL}/posts`,post)
-        console.log(response)
-        return response
+// Function to create a new post
+export async function createPost(post) {
+    console.log(post);
+    const data = await createImage(post.file);
+    const imageId = post.file.name;
+    post.imageId = imageId;
+    const response = await axios.post(`${URL}/posts`, post);
+    console.log(response);
+    return response;
 }
 
-export async function updatePost(id, post){
-    const response = await axios.put(`${URL}/posts/${id}`,post)
-    return response
+// Function to update a post by ID
+export async function updatePost(id, post) {
+    const response = await axios.put(`${URL}/posts/${id}`, post);
+    return response;
 }
 
-export async function deletePost(id){
-    const response = await axios.delete(`${URL}/posts/${id}`)
-        return response
+// Function to delete a post by ID
+export async function deletePost(id) {
+    const response = await axios.delete(`${URL}/posts/${id}`);
+    return response;
 }
 
-
-export async function userPosts(){
-    const response = await axios.get(`${URL}/users`)
-    if (response.status===200){
-    return response.data
-    } else{
-        return
+// Function to get all users
+export async function userPosts() {
+    const response = await axios.get(`${URL}/users`);
+    if (response.status === 200) {
+        return response.data;
+    } else {
+        return;
     }
 }
 
+// Function to get a specific user by ID
 export async function getUser(id) {
-        const response = await axios.get(`${URL}/users/${id}`)
-        if (response.status===200){
-        return response.data
-        } else{
-            return
-        }
+    const response = await axios.get(`${URL}/users/${id}`);
+    if (response.status === 200) {
+        return response.data;
+    } else {
+        return;
+    }
 }
 
-export async function createUser(user){
-    const response = await axios.post(`${URL}/users`,user)
-        return response
-}
-export async function loginUser(user){
-    const response = await axios.post(`${URL}/users/login`,user)
-    if(response.data.success){
-                return response.data.user
-            } 
-    else{
-                throw new Error(response.statusText)
-      }
-
-//     if(response.request.status==200){
-//         return response.data.token
-//     } else{
-//         throw new Error(response.statusText)
-//     }
+// Function to create a new user
+export async function createUser(user) {
+    const response = await axios.post(`${URL}/users`, user);
+    return response;
 }
 
-export async function updateUser(id, user){
-    const response = await axios.put(`${URL}/users/${id}`,user)
-    return response
+// Function to log in a user
+export async function loginUser(user) {
+    const response = await axios.post(`${URL}/users/login`, user);
+    if (response.data.success) {
+        return response.data.user;
+    } else {
+        throw new Error(response.statusText);
+    }
 }
+
+// Function to update a user by ID
+export async function updateUser(id, user) {
+    const response = await axios.put(`${URL}/users/${id}`, user);
+    return response;
+}
+
+// Function to upload an image (multipart/form-data)
 export async function createImage(file) {
-    const formData = new FormData()
-    formData.append('image', file)
+    const formData = new FormData();
+    formData.append('image', file);
     const response = await axios.post(`${URL}/images`, formData, {
         headers: {
             'Content-Type': 'multipart/form-data'
         }
-    })
-    return response
+    });
+    return response;
 }
 
+// Function to get an image by ID
 export async function getImage(id) {
-    const response = await axios.get(`${URL}/images/${id}`)
-    return response
+    const response = await axios.get(`${URL}/images/${id}`);
+    return response;
 }
+
+
+
+// import axios from "axios"
+// const URL= "https://mongobase-two.vercel.app/"
+// export async function getPosts(){
+//     const response = await axios.get(`${URL}/posts`)
+//     if (response.status===200){
+//     return response.data
+//     } else{
+//         return
+//     }
+// }
+
+// export async function getPost(id) {
+//     //"http://localhost:3000/posts/12345"
+//     const response = await axios.get(`${URL}/posts/${id}`)
+
+//     const post = response.data
+//     const data = await getImage(post.imageId)
+//     post.image = data
+//     return post
+// }
+
+
+// export async function createPost(post){
+//     console.log(post)
+//     const data= await createImage(post.file)
+//     const imageId = post.file.name
+//     post.imageId= imageId
+//     const response = await axios.post(`${URL}/posts`,post)
+//         console.log(response)
+//         return response
+// }
+
+// export async function updatePost(id, post){
+//     const response = await axios.put(`${URL}/posts/${id}`,post)
+//     return response
+// }
+
+// export async function deletePost(id){
+//     const response = await axios.delete(`${URL}/posts/${id}`)
+//         return response
+// }
+
+
+// export async function userPosts(){
+//     const response = await axios.get(`${URL}/users`)
+//     if (response.status===200){
+//     return response.data
+//     } else{
+//         return
+//     }
+// }
+
+// export async function getUser(id) {
+//         const response = await axios.get(`${URL}/users/${id}`)
+//         if (response.status===200){
+//         return response.data
+//         } else{
+//             return
+//         }
+// }
+
+// export async function createUser(user){
+//     const response = await axios.post(`${URL}/users`,user)
+//         return response
+// }
+// export async function loginUser(user){
+//     const response = await axios.post(`${URL}/users/login`,user)
+//     if(response.data.success){
+//                 return response.data.user
+//             } 
+//     else{
+//                 throw new Error(response.statusText)
+//       }
+
+// //     if(response.request.status==200){
+// //         return response.data.token
+// //     } else{
+// //         throw new Error(response.statusText)
+// //     }
+// }
+
+// export async function updateUser(id, user){
+//     const response = await axios.put(`${URL}/users/${id}`,user)
+//     return response
+// }
+// export async function createImage(file) {
+//     const formData = new FormData()
+//     formData.append('image', file)
+//     const response = await axios.post(`${URL}/images`, formData, {
+//         headers: {
+//             'Content-Type': 'multipart/form-data'
+//         }
+//     })
+//     return response
+// }
+
+// export async function getImage(id) {
+//     const response = await axios.get(`${URL}/images/${id}`)
+//     return response
+// }
 
